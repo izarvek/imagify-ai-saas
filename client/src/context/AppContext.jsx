62a8bd797efc9e56d8646226de960355f5320738
Vivek Sharma <viveksharmaa6307016893@gmail.come>
2026-01-16 21:53:31 +0530

    feat: add login component

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,10 +4,13 @@ export const AppContext = createContext();
 
 const AppProvider = ({children}) => {
   const [user, setUser] = useState(null);
+  const [showLogin , setShowLogin] = useState(false)
 
   const value = {
     user,
     setUser,
+    showLogin,
+    setShowLogin,
   };
 
   return (
